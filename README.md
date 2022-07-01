7e3effc4d6a08fb5bfc08972982989d1946f82b7
Matheus Fornari <matheusfornari@hotmail.com>
2022-07-01 10:24:02 -0300

    finalização do projeto

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 
+# Calculadora de IMC
+
+Projeto em React + Typescript fazendo uma calculadora de IMC.
+
+### Instalação
+- `npm install`
+
+### Para rodar
+- `npm start`
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import styles from './App.module.css';
 import poweredImage from './assets/powered.png';
+import leftArrowImage from './assets/leftarrow.png';
 import { GridItem} from './components/GridItem';
 
 import {levels, calculateImc, Level} from './helpers/imc';
@@ -16,13 +17,19 @@ const App =() => {
     } else {
       alert('Digite todos os campos');
     }    
-  }
+  };
+
+  const handleBackButton = () => {
+    setToShow(null);
+    setWeightField(0);
+    setHeightField(0);
+  };
 
   return(
     <div className={styles.main}>
       <header>
         <div className={styles.headerContainer}>
-          <img src={poweredImage} alt="" width={150} />
+          {/* <img src={poweredImage} alt="" width={150} /> */}
         </div>
       </header>
       <div className={styles.container}>
@@ -35,15 +42,19 @@ const App =() => {
             placeholder="Digite a sua altura. Ex: 1.5 (em métros)"
             value={heightField > 0 ? heightField : ''}
             onChange={e => setHeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
           />
           <input 
             type="number"
             placeholder="Digite o seu peso. Ex: 75.3 (em kg)"
             value={weightField > 0 ? weightField : ''}
             onChange={e => setWeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
           />
 
-          <button onClick={handleCalculateButton}>Calcular</button>
+          <button 
+          disabled={toShow ? true : false}
+          onClick={handleCalculateButton}>Calcular</button>
         </div>
         <div className={styles.rightSide}>
           {!toShow &&
@@ -55,12 +66,15 @@ const App =() => {
           }
           {toShow &&
           <div className={styles.rightBig}>
-            <div className={styles.rightArrow}></div>
+            <div className={styles.rightArrow} onClick={handleBackButton}>
+              <img src={leftArrowImage} width={25} alt="" />
+            </div>
             <GridItem item={toShow}/>
           </div>
           }
         </div>
       </div>
+
     </div>
   );
 }
